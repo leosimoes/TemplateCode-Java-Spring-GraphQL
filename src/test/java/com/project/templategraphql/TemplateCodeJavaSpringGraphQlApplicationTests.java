a67cbf621490ed884d6893b5fa01ccfbf59cd717
Leonardo <leosimoes.engcomp@gmail.com>
2024-04-22 14:02:43 -0300

    Project started

---
+++ src/test/java/com/project/templategraphql/TemplateCodeJavaSpringGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.templategraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateCodeJavaSpringGraphQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
